68c80ecc38ac2d709b093bdac4704a5063dd14a9
Fortier <Matthew.Fortier@allscripts.com>
2017-07-18 16:35:01 -0400

    Initial Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  title: string = '';
+  body: string = '';
+  category: string = 'Coding';
+  tags = ["tech", "coding", "terminal", "zsh"];
+
+  email: string = '';
+  password: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    
+    this.user = this.afAuth.authState;
+    if (this.user) {
+      this.items = this.af.list('/posts');    
+    }
+
+  }
+
+  login() {
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password);
+    this.items = this.af.list('/posts');    
+  }
+
+  logout() {
+      this.afAuth.auth.signOut();
+  }
+
+  Send() {
+      this.items.push({ title: this.title, body: this.body, slug: this.slugify(this.title), tags: this.tags, category: this.category });
+      this.title = '';
+      this.body = '';
+  }
+
+  slugify(text) {
+    return text.toString().toLowerCase()
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');            // Trim - from end of text
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { firebase } from '../environments/firebase.config';
+
+export const firebaseConfig = {
+  apiKey: firebase.apiKey,
+  authDomain: firebase.authDomain,
+  databaseURL: firebase.databaseURL,
+  projectId: firebase.projectId,
+  storageBucket: firebase.storageBucket,
+  messagingSenderId: firebase.messagingSenderId
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
