c01be49677bfd245d832da7aadf253de2a4c8c22
matthewfortier <matther2012@gmail.com>
2017-09-24 13:33:15 -0400

    Making the site more mobile friendly by removing space when on mobile or tablet and redesign of the mobile menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Inject } from "@angular/core";
+import { DOCUMENT } from '@angular/platform-browser';
+import { HostListener} from "@angular/core";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  scrollTop: number = 0;
+  scrollFade: boolean = false;
+  scrollMobile: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
+    if (window.innerWidth < 992)
+      this.scrollMobile = true;
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.scrollTop = this.document.body.scrollTop;
+    if (this.scrollTop > 100)
+      this.scrollFade = true;
+    else
+      this.scrollFade = false;
   }
 
 }
